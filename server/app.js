b88af161f56738f79fabbbe534fb0f0c0d3215f0
Shane Kobylecky <skobylecky1@gmail.com>
2022-10-03 15:56:14 -0700

    updated package.json

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../build');
 
 app.use(express.static(publicPath));
